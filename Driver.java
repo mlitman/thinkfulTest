389e5b5ed6efe6ffdcbf4510cc3aedbe3b5d81e6
Michael Litman <mumbl1@Michaels-iMac-2.local>
2015-03-26 15:41:48 -0500

    initial commit

---
+++ Driver.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+
+public class Driver {
+
+	public static void main(String[] args) 
+	{
+	  String s = "hello";
+	  String s3 = "hello";
+	  String s2 = new String("hello");
+	  int a = 5;
+	  int b = 5;
+	  int c = 7;
+	  
+	  System.out.println(s.equals(s2));
+	}
+}
